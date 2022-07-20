13ad486fb50222cd1b8a085f685fb5242debab5b
aghiadaboud <aghiad.aboud@gmail.com>
2022-07-20 14:45:38 +0200

    updated functions

--- cluster/fires.py
+++ cluster/fires.py
@@ -5,8 +5,8 @@ from subspaceclustering.cluster.dbscan import dbscan
 
 
 class fires:
-  
-  
+
+
   def __init__(self, data, eps,  minpts, k, mu, minClu):
     self.__data = data
     self.__eps = eps
@@ -14,7 +14,7 @@ class fires:
     self.__k = k
     self.__mu = mu
     self.__minClu = minClu
-    
+
     self.__clusters = {}
     self.__pruned_C1 = []
     self.__unpruned_C1 = []
@@ -29,40 +29,40 @@ class fires:
 
 
   def __verify_arguments(self):
-    
+
     if self.__minClu < 1:
       raise ValueError("minClu should be a natural number.")
-    
+
     if self.__minpts < 1:
       raise ValueError("minpts should be a natural number.")
-    
+
     if len(self.__data) == 0:
       raise ValueError("Input data is empty (size: '%d')." % len(self.__data))
 
 
 
-  def get_clusters(self):      
+  def get_clusters(self):
     return self.__clusters
-  
+
   def get__pruned_C1(self):
     return self.__pruned_C1
-  
+
   def get_cluster_to_dimension(self):
     return self.__cluster_to_dimension
-  
+
   def get_k_most_similar_clusters(self):
     return self.__k_most_similar_clusters
-  
+
   def get__best_merge_candidates(self):
     return self.__best_merge_candidates
-  
+
   def get__best_merge_clusters(self):
     return self.__best_merge_clusters
-  
+
   def get__subspace_cluster_approximations(self):
     return self.__subspace_cluster_approximations
-  
-  
+
+
 
   def process(self):
     self.generate_base_clusters()
@@ -76,12 +76,11 @@ class fires:
     self.prune_irrelevant_merge_candidate_of_subspace_cluster_approximations()
     self.refine_cluster_approximations()
     return self
-  
 
-  def generate_base_clusters(self): 
+
+  def generate_base_clusters(self):
     for dimension in range(len(self.__data[0])):
       column = [row[dimension] for row in self.__data]
-      column.pop(0)
       dbscan_instance = dbscan([[v] for v in column], self.__eps, self.__minpts)
       dbscan_instance = dbscan_instance.process()
       clusters = dbscan_instance.get_clusters()
@@ -89,9 +88,9 @@ class fires:
         for cluster in clusters:
           self.__unpruned_C1.append(cluster)
           self.__cluster_to_dimension[len(self.__unpruned_C1) -1] = dimension
-    
-  
-    
+
+
+
 
   def prune_irrelevant_base_clusters(self):
     _25_percent_of_s_avg = self.compute_s_avg(self.__unpruned_C1) / 4
@@ -100,36 +99,36 @@ class fires:
         self.__pruned_C1.append(self.__unpruned_C1[i])
         self.__cluster_to_dimension[len(self.__pruned_C1) -1] = self.__cluster_to_dimension.pop(i)
       else:
-        self.__cluster_to_dimension.pop(i)  
-    
-    
-  
-  @staticmethod  
+        self.__cluster_to_dimension.pop(i)
+
+
+
+  @staticmethod
   def compute_s_avg(C1):
     number_of_objects = 0
     for cluster in C1:
       number_of_objects = number_of_objects + len(cluster)
     return number_of_objects / len(C1)
-  
-  
-  
-  
+
+
+
+
   def check_for_base_clusters_splits(self):
     two_thirds_of_s_avg = 2 * self.compute_s_avg(self.__pruned_C1) / 3
     for cluster in self.__pruned_C1:
       if self.__pruned_C1.count(cluster) == 1:
         most_similar_clusters = self.compute_most_similar_cluster(cluster)
         #for msc_index in most_similar_clusters:
-          #if(len(set(self.__pruned_C1[msc_index]) & set(cluster)) >= two_thirds_of_s_avg 
+          #if(len(set(self.__pruned_C1[msc_index]) & set(cluster)) >= two_thirds_of_s_avg
             # and set(cluster).difference(set(self.__pruned_C1[msc_index])) >= two_thirds_of_s_avg):
         first_condition = len(set(self.__pruned_C1[most_similar_clusters]) & set(cluster)) >= two_thirds_of_s_avg
         second_condition = len(set(cluster).difference(set(self.__pruned_C1[most_similar_clusters]))) >= two_thirds_of_s_avg
         if(first_condition and second_condition):
           self.__split_clusters[self.__pruned_C1.index(cluster)] = list(set(self.__pruned_C1[most_similar_clusters]) & set(cluster))
-  
- 
-  
-  
+
+
+
+
   def compute_most_similar_cluster(self, cluster):
     similarities = []
     for c in self.__pruned_C1:
@@ -139,20 +138,20 @@ class fires:
         similarities.append(len(set(c) & set(cluster)))
     #return np.where(np.array(similarities) == np.max(similarities))[0]
     return similarities.index(np.max(similarities))
-  
-  
-  
-  
+
+
+
+
   def split_base_clusters(self):
     for cluster_index, clusters_intersection in self.__split_clusters.items():
       cluster = self.__pruned_C1[cluster_index].copy()
       self.__pruned_C1[cluster_index] = clusters_intersection
       self.__pruned_C1.append(list(set(cluster).difference(set(clusters_intersection))))
       self.__cluster_to_dimension[len(self.__pruned_C1) -1] = self.__cluster_to_dimension.get(cluster_index)
-  
 
-    
-    
+
+
+
   def compute_k_most_similar_clusters(self):
     if self.__k < len(self.__pruned_C1):
       similarities = []
@@ -163,15 +162,15 @@ class fires:
             similarities.append(-1)
           else:
             similarities.append(len(set(self.__pruned_C1[c1]) & set(self.__pruned_C1[c2])))
-        
+
         indices_of_sorted_similarities = sorted(range(len(similarities)), key=lambda k: similarities[k])
         self.__k_most_similar_clusters[c1] = indices_of_sorted_similarities[-self.__k:]
         similarities.clear()
     else:
       raise ValueError("k should not be greater than the number of base clusters('%d')"% len(self.__pruned_C1))
-          
-          
-  
+
+
+
   def compute_best_merge_candidates(self):
     for i in range(len(self.__pruned_C1)):
       self.__best_merge_candidates[i] = []
@@ -180,21 +179,21 @@ class fires:
       if (len(set(self.__k_most_similar_clusters.get(combination[0], [])) & set(self.__k_most_similar_clusters.get(combination[1], []))) >= self.__mu):
         self.__best_merge_candidates.get(combination[0]).append(combination[1])
         self.__best_merge_candidates.get(combination[1]).append(combination[0])
-        
+
     for key in list(self.__best_merge_candidates):
       if self.__best_merge_candidates[key] == []:
         self.__best_merge_candidates.pop(key)
-  
-    
-    
-  
-  
+
+
+
+
+
   def compute_best_merge_clusters(self):
     for cluster_index, bm_candidates in self.__best_merge_candidates.items():
       if len(bm_candidates) >= self.__minClu:
         self.__best_merge_clusters.append(cluster_index)
-    
-    
+
+
   def generate_subspace_cluster_approximations(self):
     merged_list = []
     sorted_combinations = list(combinations(self.__best_merge_clusters, r=2))
@@ -205,11 +204,11 @@ class fires:
         self.__subspace_cluster_approximations.append(merged_list.copy())
 
 
-    
-    
-  
+
+
+
   def prune_irrelevant_merge_candidate_of_subspace_cluster_approximations(self):
-    
+
     for index, approximation in enumerate(self.__subspace_cluster_approximations):
       score_approximation = self.compute_quality_of_subspace_cluster_approximation(approximation)
       score_approximation_without_base_cluster = self.compute_quality_of_approximation_without_one_cluster(approximation)
@@ -222,22 +221,22 @@ class fires:
           count_remaining_base_clusters = count_remaining_base_clusters - 1
 
         l = [x for z,x in enumerate(score_approximation_without_base_cluster) if z!=i]
-        if (score_approximation_without_base_cluster[i] > score_approximation 
+        if (score_approximation_without_base_cluster[i] > score_approximation
             and all(v <= score_approximation_without_base_cluster[i] for v in l)):
           approximation.pop(i)
-        else: 
+        else:
           i = i + 1
-  
-    
-  
+
+
+
   def compute_quality_of_subspace_cluster_approximation(self, approximation):
     cluster_dimensionality = len(approximation)
     base_clusters_of_approximation = [self.__pruned_C1[i] for i in approximation]
     cluster_size = len(set.intersection(*map(set,base_clusters_of_approximation)))
     return sqrt(cluster_size) * cluster_dimensionality
-  
-  
-  
+
+
+
   def compute_quality_of_approximation_without_one_cluster(self, approximation):
     score_approximation_without_base_cluster = []
     for cluster_index in approximation:
@@ -246,12 +245,12 @@ class fires:
       score = self.compute_quality_of_subspace_cluster_approximation(temp_approximation)
       score_approximation_without_base_cluster.append(score)
     return score_approximation_without_base_cluster
-  
-  
-  
-  
+
+
+
+
   def refine_cluster_approximations(self):
-    
+
     for approximation in self.__subspace_cluster_approximations:
       features = [self.__cluster_to_dimension.get(k) for k in approximation]
       features = list(set(features))
@@ -265,24 +264,22 @@ class fires:
       subspace_clusters = dbscan_instance.get_clusters()
       if subspace_clusters:
         self.__clusters[tuple(features)] = subspace_clusters.copy()
-  
-  
-  
-    
+
+
+
+
   def get_cluster_members_values(self, points, features):
     temp_data = np.array(self.__data)
-    data_without_features_row = np.delete(temp_data, 0, 0)
-    newValues = data_without_features_row[np.ix_(points,features)]
+    newValues = temp_data[np.ix_(points,features)]
     return newValues.tolist()
-  
-  
+
+
   def adjust_density_threshold(self, n, d):
     return (self.__eps * n) / (pow(n,(1/d)))
-  
-  
-  
-  
-  
-  
-  
-  
+
+
+
+
+
+
+
--- cluster/subclu.py
+++ cluster/subclu.py
@@ -5,48 +5,47 @@ from more_itertools import locate
 from subspaceclustering.cluster.dbscan import dbscan
 
 class subclu:
-  
+
   def __init__(self, data, eps, m):
     self.__data = data
     self.__eps = eps
     self.__m = m
-        
+
     self.__S1_indices = []  #set of 1-D subspaces containing clusters
     self.__C1 = {}  # set of all sets of clusters in 1-D subspaces
     self.__clusters = {}
 
     self.__verify_arguments()
-    
-    
-    
+
+
+
   def __verify_arguments(self):
-    
+
     if self.__m < 1:
       raise ValueError("minpts should be a natural number.")
 
     if len(self.__data) == 0:
       raise ValueError("Input data is empty (size: '%d')." % len(self.__data))
-      
+
     if np.min(self.__data) < 0:
       raise ValueError("All values should be greater or equal to 0.")
-      
-      
-      
-  def get_clusters(self):      
+
+
+
+  def get_clusters(self):
     return self.__clusters
 
 
-  
+
   def process(self):
     self.generate_all_1_D_clusters()
     self.generate_kplus1_D_clusters_from_k_D_clusters()
     return self
-  
-  
+
+
   def generate_all_1_D_clusters(self):
     for subspace in range(len(self.__data[0])):
       column = [row[subspace] for row in self.__data]
-      column.pop(0)
       dbscan_instance = dbscan([[v] for v in column], self.__eps, self.__m)
       dbscan_instance = dbscan_instance.process()
       subspace_clusters = dbscan_instance.get_clusters()
@@ -54,10 +53,10 @@ class subclu:
         self.__S1_indices.append(subspace)
         self.__C1[subspace] = subspace_clusters.copy()
     self.__clusters = self.__C1.copy()
-        
-        
-      
-      
+
+
+
+
   def generate_kplus1_D_clusters_from_k_D_clusters(self):
     k = 1
     Ck = self.__C1.copy()
@@ -71,7 +70,7 @@ class subclu:
         c_cand = []
         for bestSubspace in bestSubspaces:
           for cluster in self.__clusters.get(bestSubspace):
-            points = self.get_cluster_members_values(cluster, cand)     
+            points = self.get_cluster_members_values(cluster, cand)
             dbscan_instance = dbscan(points, self.__eps, self.__m)
             dbscan_instance = dbscan_instance.process()
             for cluster_in_higher_dim in dbscan_instance.get_clusters():
@@ -83,9 +82,9 @@ class subclu:
       self.__clusters = self.__clusters | Ck.copy()
       k = k + 1
 
-    
-    
-    
+
+
+
   def generate_candidate_subspaces(self, Sk, k):
     candSkplus1 = []
     if k == 1:
@@ -98,9 +97,9 @@ class subclu:
             candSkplus1.append(s1.copy() + [s2[-1]])
       self.prune_irrelevant_candidates_subspaces(candSkplus1, Sk, k)
     return candSkplus1
-            
-  
-  
+
+
+
   def prune_irrelevant_candidates_subspaces(self, candSkplus1, Sk, k):
     for cand in candSkplus1:
       sorted_combinations = list(combinations(cand, r=k))
@@ -108,10 +107,10 @@ class subclu:
       for subspace in list_combinations:
         if subspace not in Sk:
           candSkplus1.remove(cand)
-  
-  
-  
-  
+
+
+
+
   def find_min_cluster(self, cand, k, consider_all_bestsubspaces = False):
     cand_combinations = cand.copy()
     if k > 1:
@@ -124,27 +123,25 @@ class subclu:
       for cluster in clusters:
         count_objects = count_objects + len(cluster)
       size.append(count_objects)
-      
+
     if consider_all_bestsubspaces == False:
       min_cluster_index = size.index(np.min(size))
       bestsubspace = [cand_combinations[min_cluster_index]]
     else:
       min_clusters_indcies = list(locate(size, lambda a: a == np.min(size)))
       bestsubspace = [cand_combinations[i] for i in min_clusters_indcies]
-  
+
     return bestsubspace
-     
-       
+
+
 
   def get_cluster_members_values(self, points, features):
     temp_data = np.array(self.__data)
-    data_without_features_row = np.delete(temp_data, 0, 0)
-    newValues = data_without_features_row[np.ix_(points,features)]
-    return newValues.tolist()    
-    
+    newValues = temp_data[np.ix_(points,features)]
+    return newValues.tolist()
 
 
-  
-  
-        
-    
+
+
+
+
